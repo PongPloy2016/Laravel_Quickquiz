f075eafd22afe9e4d62a9e9f78ed8bbad9485852
PongPloyProgramDev <pongku71@gmail.com>
2026-07-18 00:11:12 +0700

    fix: add DATABASE_URL connection URL for pgsql

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'quiz_db'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'root'),
+            'host' => env('DB_HOST', 'dpg-d9d61hurnols73cue66g-a'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'my_3py7'),
+            'username' => env('DB_USERNAME', 'my_3py7_user'),
+            'password' => env('DB_PASSWORD', '2RA7Oty67pkYNkuhwunWUg0QYP2djX8i'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
